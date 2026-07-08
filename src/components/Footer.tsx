e0f17964cfe78fdc5c552d02259b96d63434bb62
Adarsh <140302686+Aadarsh6@users.noreply.github.com>
2026-07-08 12:06:26 +0530

    minor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
+import { socials } from "../Data/Socials";
+
 function Footer() {
+    const github = socials.find((social)=> social.label === "GitHub")
   return (
-    <footer className="border-t border-black/5 px-6 py-8 text-center text-sm text-text-light/40 dark:border-white/10 dark:text-text-dark/40">
-      <p>© {new Date().getFullYear()} Aadarsh Mishra. Built with React &amp; Tailwind.</p>
+    <footer className="border-t border-black/5 px-6 py-8 flex justify-center gap-2 text-center text-sm text-text-light/40 dark:border-white/10 dark:text-text-dark/40">
+      <p>© {new Date().getFullYear()} </p>
+        
+        <a 
+        href={github?.href}
+        target= "_blank"
+        rel="noopener noreferrer"
+           className="transition-colors hover:text-secondary dark:text-accent underline"
+        >Aadarsh Mishra.</a>
+
     </footer>
   );
 }
